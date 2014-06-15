f06a0c350619a8d35467adde3b696d80055fa154
jimmylin192 <linxin@gmail.com>
2014-06-14 21:38:34 -0500

    computing framework

--- codebase/sparseClustering/util.h
+++ codebase/sparseClustering/util.h
@@ -4,7 +4,7 @@ using namespace std;
 const double INF = 1e300;
 
 template <class T>
-void shuffle( vector<T>& vec ){
+void shuffle( vector<T>& vec ) {
 
 	int l = vec.size();
 	int j;
@@ -16,7 +16,7 @@ void shuffle( vector<T>& vec ){
 }
 
 
-double dot(vector<double>& v, vector<pair<int,double> >& f){
+double dot(vector<double>& v, vector<pair<int,double> >& f) {
 	
 	double sum = 0.0;
 	for(int i=0;i<f.size();i++)
@@ -25,7 +25,7 @@ double dot(vector<double>& v, vector<pair<int,double> >& f){
 	return sum;
 }
 
-double dot(double* v, vector<pair<int,double> >& f){
+double dot(double* v, vector<pair<int,double> >& f) {
 	
 	double sum = 0.0;
 	for(int i=0;i<f.size();i++)
@@ -34,39 +34,53 @@ double dot(double* v, vector<pair<int,double> >& f){
 	return sum;
 }
 
-double dot(double* v, double* w, int d){
+double dot(double* v, double* w, int d) {
 	
 	double sum = 0.0;
-	for(int i=0;i<d;i++){
+	for(int i=0;i<d;i++) {
 		sum += v[i]*w[i];
 	}
 	return sum;
 }
 
-void vadd(double* v, vector<pair<int,double> >& f){
+void vadd(double* v, vector<pair<int,double> >& f) {
 	
 	for(int i=0;i<f.size();i++)
 		v[ f[i].first ] += f[i].second;
 }
 
-void vadd(vector<double>& vec, vector<pair<int,double> >& f){
+void vadd(vector<double>& vec, vector<pair<int,double> >& f) {
 	
 	for(int i=0;i<f.size();i++)
 		vec[ f[i].first ] += f[i].second;
 }
 
-void vsub(double* v, vector<pair<int,double> >& f){
+void vsub(double* v, vector<pair<int,double> >& f) {
 	
 	for(int i=0;i<f.size();i++)
 		v[ f[i].first ] -= f[i].second;
 }
 
-int max_index(double* arr, int size){
+int max_index (vector<double>& vec) {
+
+    double vmax = -INF; 
+    int imax;
+    for (int i = 0; i < vec.size(); i ++) {
+        if (vec[i] > vmax) {
+            vmax = vec[i];
+            imax = i;
+        }
+    }
+
+    return imax;
+}
+
+int max_index (double* arr, int size) {
 	
 	double vmax= -INF;
 	int imax;
-	for(int i=0;i<size;i++){
-		if( arr[i] > vmax ){
+	for(int i=0;i<size;i++) {
+		if( arr[i] > vmax ) {
 			vmax = arr[i];
 			imax = i;
 		}
@@ -74,11 +88,11 @@ int max_index(double* arr, int size){
 	return imax;
 }
 
-double flip(double a){
+double flip (double a) {
 	
 	return (a==0.0)?1.0:0.0;
 }
 
-int flip(int a){
+int flip (int a) {
 	return (a==0)?1:0;
 }
