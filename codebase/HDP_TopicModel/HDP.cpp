9033cd5815407abf70f9148460292c2b4d461f98
jimmylin192 <linxin@gmail.com>
2014-07-25 14:18:11 -0400

    apply exSparseMat on HDP problem

--- codebase/HDP_TopicModel/HDP.cpp
+++ codebase/HDP_TopicModel/HDP.cpp
@@ -27,11 +27,9 @@
 
 
 double sign (int input) {
-
     if (input > 0) return 1.0;
     else if ( input < 0 ) return -1.0;
     else return 0.0;
-
 }
 
 bool pairComparator (const std::pair<int, double>& firstElem, const std::pair<int, double>& secondElem) {
@@ -40,14 +38,13 @@ bool pairComparator (const std::pair<int, double>& firstElem, const std::pair<in
 }
 
 typedef double (* dist_func) (Instance*, Instance*, int); 
-const double r = 1000.0;
 
+/*{{{*/
+/*
 int get_nCentroids (double ** W, int nRows, int nCols) {
 
     int nTopics = 0;
 
-/*{{{*/
-    /*
     double * sum_belonging = new double [nCols];
 
     mat_max_col (W, sum_belonging, nRows, nCols);
@@ -59,8 +56,6 @@ int get_nCentroids (double ** W, int nRows, int nCols) {
     }
     
     delete[] sum_belonging;
-    */
-/*}}}*/
 
     return nTopics;
 }
@@ -69,8 +64,6 @@ vector<int> get_all_centroids(double ** W, int nRows, int nCols) {
 
     std::vector<int> topics;
 
-/*{{{*/
-    /*
     double * sum_belonging = new double [nCols];
     for (int i = 0; i < nCols; i ++) {
         sum_belonging[i] = 0.0;
@@ -85,42 +78,39 @@ vector<int> get_all_centroids(double ** W, int nRows, int nCols) {
     }
     
     delete[] sum_belonging;
-    */
-/*}}}*/
 
     return topics;
 }
+*/
+/*}}}*/
 
-double first_subproblm_obj (double ** dist_mat, double ** yone, double ** zone, double ** wone, double rho, int N) {
+double first_subproblm_obj (Esmat* Y_1, Esmat* Z_1, Esmat* W_1, double RHO, int N) {
 
-    double ** temp = mat_init (N, N);
-    double ** diffone =mat_init (N, N);
-    mat_zeros (diffone, N, N);
+    Esmat* temp = esmat_init (N, N);
+    Esmat* diffone = esmat_init (N, N);
 
     // sum1 = 0.5 * sum_n sum_k (w_nk * d^2_nk) -> loss
-    mat_zeros (temp, N, N);
-    mat_times (wone, dist_mat, temp, N, N);
-    double sum1 = 0.5 * mat_sum (temp, N, N);
+    /* this term do not exist since we did not consider noise
+    esmat_times (W_1, dist_mat, temp);
+    double sum1 = 0.5 * esmat_sum (temp);
+    */
 
     // sum2 = y_1^T dot (w_1 - z) -> linear
-    mat_zeros (temp, N, N);
-    mat_sub (wone, zone, diffone, N, N); // temp = w_1 - z_1
-    mat_tdot (yone, diffone, temp, N, N);
-    double sum2 = mat_sum (temp, N, N);
+    esmat_zeros (temp);
+    esmat_sub (W_1, Z_1, diffone); // temp = w_1 - z_1
+    esmat_fdot (Y_1, diffone, temp);
+    double linear = esmat_sum (temp);
 
-    // sum3 = 0.5 * rho * || w_1 - z_1 ||^2 -> quadratic
-    mat_zeros (temp, N, N);
-    mat_sub (wone, zone, temp, N, N);
-    double sum3 = 0.5 * rho * mat_norm2 (temp, N, N);
+    // sum3 = 0.5 * RHO * || w_1 - z_1 ||^2 -> quadratic
+    esmat_zeros (temp);
+    esmat_sub (W_1, Z_1, temp);
+    double quadratic = 0.5 * RHO * esmat_fnorm (temp);
 
-    // sum4 = r dot (1 - sum_k w_nk) -> dummy
-    double * temp_vec = new double [N];
-    mat_sum_row (wone, temp_vec, N, N);
-    double dummy_penalty = 0.0;
-    for (int i = 0; i < N; i ++) {
-        dummy_penalty += r*(1 - temp_vec[i]);
-    }
-    double total = sum1+sum2+sum3+dummy_penalty;
+    // dummy_penalty = r dot (1 - sum_k w_nk)
+    Esmat temp_vec = esmat_init (W_1->nRows, W_1->nCols);
+    esmat_sum_row (W_1, temp_vec);
+    double dummy = esmat_compute_dummy (temp_vec);
+    double total = linear+qudratic+dummy;
     /*
 #ifdef FRANK_WOLFE_DUMP
     cout << "[Frank_wolfe] (loss, linear, quadratic, dummy, total) = (" 
@@ -128,56 +118,52 @@ double first_subproblm_obj (double ** dist_mat, double ** yone, double ** zone, 
          <<  ")" << endl;
 #endif
 */
-
-    mat_free (temp, N, N);
-    mat_free (diffone, N, N);
-    delete [] temp_vec;
+    esmat_free (temp);
+    esmat_free (diffone);
+    esmat_free (temp_vec);
 
     return total;
 }
 
-void frank_wolfe_solver (double ** MATCH_MAT, double ** Y_1, double ** Z_1, double ** w_1, double RHO, int N) {
+void frank_wolfe_solver (Esmat * Y_1, Esmat * Z_1, Esmat * w_1, double RHO, int N) {
 
     bool is_global_optimal_reached = false;
 
     // cout << "within frank_wolfe_solver" << endl;
     // This can be computed by using corner point. 
-    double ** gradient = mat_init (N, N);
-    double ** s = mat_init (N, N);
-    mat_zeros (gradient, N, N);
+    Esmat * gradient = esmat_init (w_1);
+    Esmat * s = esmat_init (w_1);
 
 #ifndef EXACT_LINE_SEARCH
     int K = 300;
 #else
     int K = 2;
-    double ** w_minus_s = mat_init (N, N);
-    double ** w_minus_z = mat_init (N, N);
+    Esmat * w_minus_s = esmat_init (w_1);
+    Esmat * w_minus_z = esmat_init (w_1);
 #endif
 
     int k = 0; // iteration number
     double gamma; // step size
     double penalty;
-    double ** tempS = mat_init (N, N);
+    Esmat * tempS = esmat_init (w_1);
 
-    //mat_zeros (w_1, N, N);
+    //esmat_zeros (w_1);
     double sum1, sum2, sum3, sum4;
     // cout << "within frank_wolfe_solver: start iteration" << endl;
     while (k < K && !is_global_optimal_reached) {
-        // STEP ONE: find s minimize <s, grad f>
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                gradient[i][j] =  0.5*MATCH_MAT[i][j] + Y_1[i][j] + RHO * (w_1[i][j] - Z_1[i][j]) ;  //- r[i];
-            }
-        }
-        mat_zeros (s, N, N);
-        mat_min_row (gradient, s, N, N);
+        // STEP ONE: find s that minimizes <s, grad f>
+        // gradient[i][j] = Y_1[i][j] + RHO * (w_1[i][j] - Z_1[i][j]) ;  //- r[i];
+        esmat_sub (w_1, Z_1, tempS);
+        esmat_scalar_mult (RHO, tempS);
+        esmat_add (Y_1, tempS, gradient);
+        esmat_min_row (gradient, s);
 
         /*
 #ifdef FRANK_WOLFE_DUMP
-        cout << "mat_norm2 (w_1, N, N): " <<  mat_norm2 (w_1, N, N) << endl;
-        cout << "mat_norm2 (Y_1, N, N): " <<  mat_norm2 (Y_1, N, N) << endl;
-        cout << "mat_norm2 (gradient, N, N): " <<  mat_norm2 (gradient, N, N) << endl;
-        cout << "mat_sum (s, N, N): " <<  mat_sum (s, N, N) << endl;
+        cout << "esmat_norm2 (w_1): " <<  esmat_norm2 (w_1) << endl;
+        cout << "esmat_norm2 (Y_1): " <<  esmat_norm2 (Y_1) << endl;
+        cout << "esmat_norm2 (gradient): " <<  esmat_norm2 (gradient) << endl;
+        cout << "esmat_sum (s): " <<  esmat_sum (s) << endl;
 #endif
 */
         // cout << "within frank_wolfe_solver: step one finished" << endl;
@@ -196,39 +182,30 @@ void frank_wolfe_solver (double ** MATCH_MAT, double ** Y_1, double ** Z_1, doub
         // sum2 = sum_n sum_k (w - s)_nk
         // sum3 = - RHO * sum_n sum_k  (w - z) 
         // sum4 = sum_n sum_k RHO * (s - w)
-        mat_zeros (tempS, N, N);
-        mat_zeros (w_minus_s, N, N);
-        mat_zeros (w_minus_z, N, N);
-        mat_sub (w_1, s, w_minus_s, N, N);
-        mat_sub (w_1, Z_1, w_minus_z, N, N);
+        esmat_sub (w_1, s, w_minus_s);
+        esmat_sub (w_1, Z_1, w_minus_z);
 
         // NOTE: in case of ||w_1 - s||^2 = 0, not need to optimize anymore
         // since incremental term = w + gamma (s - w), and whatever gamma is,
         // w^(k+1) = w^(k), this would be equivalent to gamma = 0
-        if (mat_norm2(w_minus_s, N, N) == 0) {
+        if (esmat_fnorm(w_minus_s) == 0) {
             gamma = 0;
             is_global_optimal_reached = true;
+            // reach the exit condition, do not make more iteration
         } else {
-            mat_times (w_minus_s, dist_mat, tempS, N, N);
-            sum1 = 0.5 * mat_sum (tempS, N, N);
-
-            mat_zeros (tempS, N, N);
-            mat_tdot (Y_1, w_minus_s, tempS, N, N);
-            sum2 = mat_sum (tempS, N, N);
- 
-            mat_zeros (tempS, N, N);
-            mat_tdot (w_minus_z, w_minus_s, tempS, N, N);
-            sum3 = RHO * mat_sum (tempS, N, N);
-
-            mat_zeros (tempS, N, N);
-            sum4 = RHO * mat_norm2 (w_minus_s, N, N);
-
+            /*
+            esmat_fdot (w_minus_s, dist_mat, tempS);
+            sum1 = 0.5 * esmat_sum (tempS);
+            */
+            sum2 = esmat_fdot (Y_1, w_minus_s);
+            sum3 = RHO * esmat_fdot (w_minus_z, w_minus_s);
+            sum4 = RHO * esmat_fnorm (w_minus_s);
             // gamma should be within interval [0,1]
             gamma = (sum1 + sum2 + sum3) / sum4;
 
 #ifdef FRANK_WOLFE_DUMP
-            cout << "mat_norm2 (w_minus_s, N, N)" << mat_norm2 (w_minus_s, N, N) << endl;
-            cout << "mat_norm2 (w_minus_z, N, N)" << mat_norm2 (w_minus_z, N, N) << endl;
+            cout << "esmat_norm2 (w_minus_s)" << esmat_fnorm (w_minus_s) << endl;
+            cout << "esmat_norm2 (w_minus_z)" << esmat_fnorm (w_minus_z) << endl;
 #endif
             // cout << "within frank_wolfe_solver: step two finished" << endl;
 
@@ -243,13 +220,13 @@ void frank_wolfe_solver (double ** MATCH_MAT, double ** Y_1, double ** Z_1, doub
         }
 #endif
         // update the w^(k+1)
-        mat_zeros (tempS, N, N);
-        mat_dot (gamma, s, tempS, N, N);
-        mat_dot (1.0-gamma, w_1, w_1, N, N);
-        mat_add (w_1, tempS, w_1, N, N);
+        esmat_scalar_mult (gamma, s); 
+        esmat_scalar_mult (1.0-gamma, w_1);
+        esmat_copy (w_1, tempS);
+        esmat_add (tempS, s, w_1);
 
         // compute value of objective function
-        penalty = first_subproblm_obj (dist_mat, Y_1, Z_1, w_1, RHO, N);
+        penalty = first_subproblm_obj (Y_1, Z_1, w_1, RHO, N);
         // cout << "within frank_wolfe_solver: step three finished" << endl;
         // report the #iter and objective function
         /*
@@ -260,51 +237,31 @@ void frank_wolfe_solver (double ** MATCH_MAT, double ** Y_1, double ** Z_1, doub
     // cout << "within frank_wolfe_solver: next iteration" << endl;
     }
     // cout << "within frank_wolfe_solver: to free gradient" << endl;
-    mat_free (gradient, N, N);
+    esmat_free (gradient);
     // cout << "within frank_wolfe_solver: to free temps" << endl;
-    mat_free (tempS, N, N);
+    esmat_free (tempS);
     // cout << "within frank_wolfe_solver: to free s " << endl;
-    mat_free (s, N, N);
+    esmat_free (s);
     // cout << "end frank_wolfe_solver: finished! " << endl;
 }
 
-
-double second_subproblem_obj (double ** ytwo, double ** z, double ** wtwo, double rho, int N, double lambda) {
-
-    double ** temp = mat_init (N, N);
-    double ** difftwo = mat_init (N, N);
-    mat_zeros (difftwo, N, N);
-
+double second_subproblem_obj (Esmat* Y_2, Esmat* Z, Esmat* W_2, double RHO, int N, double lambda) {
     // reg = 0.5 * sum_k max_n | w_nk |  -> group-lasso
-    mat_zeros (temp, N, N);
-    double * maxn = new double [N]; 
-    for (int i = 0; i < N; i ++) { // Ian: need initial 
-        maxn[i] = -INF;
-    }
+    Esmat* absW = esmat_init (W_2);
+    Esmat* maxn = esmat_init (1, W_2->nCols);
+    Esmat* sumk = esmat_init (1, 1);
+    esmat_abs (W_2, absW);
+    esmat_max_col (absW, maxn);
+    esmat_sum_row (maxn, sumk);
+    double group_lasso = lambda * sumk->val[0].second; 
 
-    for (int i = 0; i < N; i ++) {
-        for (int j = 0; j < N; j ++) {
-            if (wtwo[i][j] > maxn[j])
-                maxn[j] = wtwo[i][j];
-        }
-    }
-    double sumk = 0.0;
-    for (int i = 0; i < N; i ++) {
-        sumk += maxn[i];
-    }
-    double group_lasso = lambda * sumk; 
+    // sum2 = y_2^T dot (w_2 - Z) -> linear
+    Esmat* w_minus_z = esmat_init (W_2);
+    esmat_sub (W_2, Z, w_minus_z);
+    double linear = esmat_fdot (Y_2, w_minus_z);
 
-    // sum2 = y_2^T dot (w_2 - z) -> linear
-    mat_zeros (temp, N, N);
-    mat_sub (ytwo, z, difftwo, N, N);
-    mat_tdot (ytwo, difftwo, temp, N, N);
-    double sum2 = mat_sum (temp, N, N);
-
-    // sum3 = 0.5 * rho * || w_2 - z_2 ||^2 -> quadratic mat_zeros (temp, N, N);
-    mat_sub (wtwo, z, temp, N, N);
-    double sum3 = 0.5 * rho * mat_norm2 (temp, N, N);
-
-    mat_free (temp, N, N);
+    // sum3 = 0.5 * rho * || w_2 - Z ||^2 -> quadratic 
+    double quadratic = 0.5 * RHO * esmat_fnorm (w_minus_z);
 
     // ouput values of each components
 #ifdef BLOCKWISE_DUMP
@@ -313,18 +270,22 @@ double second_subproblem_obj (double ** ytwo, double ** z, double ** wtwo, doubl
         << ")" << endl;
 #endif
 
+    esmat_free (w_minus_z);
+    esmat_free (sumk);
+    esmat_free (maxn);
+    esmat_free (absW);
     //cerr << group_lasso << ", " << sum2 << ", " << sum3 << endl;
-    return group_lasso + sum2 + sum3;
+    return group_lasso + linear + quadratic;
 }
 
-void group_lasso_solver (double ** ytwo, double ** ztwo, double ** wtwo, double rho, double lambda, int N) {
+void group_lasso_solver (Esmat* Y_2, Esmat* Z, Esmat* w_2, double RHO, double lambda, int N) {
 
     // STEP ONE: compute the optimal solution for truncated problem
-    double ** wbar = mat_init (N, N);
-    mat_zeros (wbar, N, N);
-    mat_dot (rho, ztwo, wbar, N, N); // wbar = rho * z_2
-    mat_sub (wbar, ytwo, wbar, N, N); // wbar = rho * z_2 - y_2
-    mat_dot (1.0/rho, wbar, wbar, N, N); // wbar = (rho * z_2 - y_2) / rho
+    Esmat* wbar = esmat_init (N, N);
+    esmat_zeros (wbar);
+    esmat_dot (RHO, Z, wbar); // wbar = RHO * z_2
+    esmat_sub (wbar, Y_2, wbar); // wbar = RHO * z_2 - y_2
+    esmat_dot (1.0/RHO, wbar, wbar); // wbar = (RHO * z_2 - y_2) / RHO
 
     // STEP TWO: find the closed-form solution for second subproblem
     for (int j = 0; j < N; j ++) {
@@ -363,10 +324,10 @@ void group_lasso_solver (double ** ytwo, double ** ztwo, double ** wtwo, double 
             }
         }
 
-        // 4. assign closed-form solution to wtwo
+        // 4. assign closed-form solution to w_2
         if( max_term < 0 ){
             for(int i=0;i<N;i++)
-                wtwo[i][j] = 0.0;
+                w_2[i][j] = 0.0;
             continue;
         }
 
@@ -374,22 +335,22 @@ void group_lasso_solver (double ** ytwo, double ** ztwo, double ** wtwo, double 
             // harness vector of pair
             double value = wbar[i][j];
             if ( abs(value) >= separator ) {
-                wtwo[i][j] = max_term;
+                w_2[i][j] = max_term;
             } else {
                 // its ranking is above m*, directly inherit the wbar
-                wtwo[i][j] = max(wbar[i][j],0.0);
+                w_2[i][j] = max(wbar[i][j],0.0);
             }
         }
     }
 
     // compute value of objective function
-    double penalty = second_subproblem_obj (ytwo, ztwo, wtwo, rho, N, lambda);
+    double penalty = second_subproblem_obj (Y_2, Z, w_2, RHO, N, lambda);
     // report the #iter and objective function
     /*cout << "[Blockwise] second_subproblem_obj: " << penalty << endl;
       cout << endl;*/
 
     // STEP THREE: recollect temporary variable - wbar
-    mat_free (wbar, N, N);
+    esmat_free (wbar);
 }
 
 /* TODO: 
@@ -451,6 +412,7 @@ double opt_objective (double ** dist_mat, double lambda, int N, double ** z) {
 }
 
 /* Compute the mutual distance of input instances contained within "data" */
+/*
 void compute_match_mat (vector<Instance*>& data, double ** dist_mat, int N, int D, dist_func df, bool isSym) {
 
     // STEP ONE: convert input sparse data to desired format
@@ -459,13 +421,11 @@ void compute_match_mat (vector<Instance*>& data, double ** dist_mat, int N, int 
             ;
         }
     }
-
-    // STEP two: check correctness by assertion
-    
-
+    // STEP TWO: check correctness by assertion
 }
+*/
 
-void HDP ( double ** dist_mat, int D, int N, vector<double> LAMBDAs, double ** W) {
+void HDP (int D, int N, vector<double> LAMBDAs, double ** W) {
 
     // SET MODEL-RELEVANT PARAMETERS 
     assert (LAMBDAs.size == 2);
@@ -514,7 +474,7 @@ void HDP ( double ** dist_mat, int D, int N, vector<double> LAMBDAs, double ** W
 
         // STEP ONE: RESOLVE W_1, W_2, W_3, W_4
         // resolve w_1
-        frank_wolfe_solver (dist_mat, y_1, z, w_1, rho, N); 
+        frank_wolfe_solver ( y_1, z, w_1, rho, N); 
 
         /*
 #ifdef ITERATION_TRACE_DUMP
@@ -667,8 +627,7 @@ int main (int argc, char ** argv) {
     */
 
     // Run sparse convex clustering
-    Esmat* W = mat_init (N, N);
-    mat_zeros (W, N, N);
+    Esmat* W = esmat_init (N, 1);
     HDP (dist_mat, D, N, lambda, W);
 
     // Output results
