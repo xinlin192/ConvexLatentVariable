065eb2464baa1b7f5059afe3c1e96eb6cbed5b84
En-Hsu Yen <a061105@gmail.com>
2014-10-05 14:40:12 -0500

    LP_HDP modified

--- codebase/LP_HDP/script.sh
+++ codebase/LP_HDP/script.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-datafile=$1;
-lambda=$2;
+datafile=$1
+dataset_assign=$2
+theta=$3
+lambda=$4
 
 echo $datafile;
+echo $dataset_assign;
+echo $theta;
 echo $lambda;
 
-java GenLP $datafile $lambda;
+java GenLP $datafile $dataset_assign $theta $lambda;
 matlab < solveLP.m;
 java NameMap sol varName;
