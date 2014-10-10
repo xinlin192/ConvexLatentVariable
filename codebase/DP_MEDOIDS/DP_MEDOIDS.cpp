3660cd007f6fba6740b6c0105dcbd5de1f81d767
jimmylin192 <linxin@gmail.com>
2014-10-10 03:26:07 -0500

    DP_MEDOIDS corrected, now consistent with DP_MEAN

--- codebase/DP_MEDOIDS/DP_MEDOIDS.cpp
+++ codebase/DP_MEDOIDS/DP_MEDOIDS.cpp
@@ -13,104 +13,55 @@ double DP_MEDOIDS (double** dist_mat, int N, double lambda, double** W, vector<i
     // STEP ONE: a. pick up the optimal *global* medoid for initialization
     vector<int> new_medoids (1,0);
     double * sum_col = new double [N];
-    mat_sum_col (W, sum_col, N, N);
+    mat_sum_col (dist_mat, sum_col, N, N);
     double min_sum = 1e300;
     int min_sum_index = -1;
-    for (int i = 0; i < N; i ++) {
+    for (int i = 0; i < N; i ++) 
        if (sum_col[i] < min_sum) {
            min_sum = sum_col[i];
            min_sum_index = i;
        }
-    }
+    delete[] sum_col;
     if (min_sum_index >= 0) 
         new_medoids[0] = min_sum_index;
     else {
         cerr << "Init: optimal global medoids not found!" << endl;
         assert (false); 
     }
-    delete[] sum_col;
     cout << "optimal global medoid: " << new_medoids[0] << endl;
     vector<int> last_medoids (new_medoids);
     // OPTIONAL: write randomized medoids to stdout or external files
-    double last_cost = INF, new_cost = INF; // compute last cost
-    double ** w = mat_init (N,N);
-    for (int i = 0; i < N; i ++) 
-        w[i][last_medoids[0]] = 1.0;
+    double last_cost = min_sum, new_cost = min_sum; 
+    vector<int> z (N, 0);
     while (true) {
         // STEP TWO: compute dist square to new medoids d_ic
-        int last_K = last_medoids.size();
-        int K = new_medoids.size(); 
-        double ** temp = mat_init (N,K); // temp[i][c] <- d_ic
-        for (int i = 0; i < K; i ++) 
-            for (int j = 0; j < N; j ++) 
-                temp[j][i] = dist_mat[j][new_medoids[i]];
-        // STEP THREE: create new cluster medoid if min_c d_ic > lambda
-        // for here, we make the condition squared:
-        //           min_c d_ic^2 > lambda^2
-        double* min_d_ic = new double [N];
-        mat_min_row (temp, min_d_ic, N,K);
-        bool cont = false;
-        vector<int> candiates;
+        vector< vector<int> > members (new_medoids.size(), vector<int>());
         for (int i = 0; i < N; i ++) {
-            if (min_d_ic[i] > lambda) {
-                candiates.push_back(i); // add to candidate list
-            }
-        }
-        int nCandidates = candiates.size();
-        if (nCandidates > 0) {
-            mat_free(temp, N,K);
-            int new_med = rand() % nCandidates;
-            new_medoids.push_back(candiates[new_med]);
-            K = new_medoids.size();
-            temp = mat_init (N,K);
-            for (int i = 0; i < K; i ++) {
-                for (int j = 0; j < N; j ++) {
-                    temp[j][i] = dist_mat[j][new_medoids[i]];
+            int nClusters = new_medoids.size(); 
+            vector<double> d_ic (nClusters, 0.0);
+            for (int c = 0; c < nClusters; c ++) 
+                d_ic[c] = dist_mat[i][new_medoids[c]];
+            // STEP THREE: create new medoid if min_c d_ic^2 > lambda
+            int min_c = -1;
+            double min_dic = INF;
+            for (int c = 0; c < nClusters; c ++) 
+                if (d_ic[c] < min_dic) {
+                    min_c = c;
+                    min_dic = d_ic[c];
                 }
+            if (d_ic[min_c] <= lambda) {
+                z[i] = min_c;
+                members[min_c].push_back(i);
+            } else {
+                new_medoids.push_back(i);
+                z[i] = nClusters;
+                members.push_back(vector<int>(1, i));
             }
         }
-        double* min_index = new double [N];
-        mat_min_index_row (temp, min_index, N,K);
-        mat_zeros(w, N,N);
-        for (int i = 0; i < N; i ++) {
-            // reassign to one of new medoids
-            w[i][new_medoids[min_index[i]]] = 1.0;
-        }
-        delete[] min_index;
-        delete[] min_d_ic;
-        mat_free(temp, N,K);
-
-        // STEP THREE: compute cost
-        new_cost = 0.5 * mat_frob_dot (w, dist_mat, N,N)+ K * lambda ;
-        cout << "new_cost: " << new_cost << endl;
-        if( new_cost < objmin )
-            objmin = new_cost;
-        objmin_trace << omp_get_wtime()-start_time << " " << objmin << endl;
-        // STEP FOUR: stopping criteria
-        if (new_cost >= last_cost && last_K == K) {
-            cout << "CLUSTERING COST: " << last_cost << endl;
-            cout << "Resulted medoids: ";
-            for (int i = 0; i < last_K; i++) {
-                cout << last_medoids[i] << ",";
-            }
-            cout << endl;
-            break;
-        } // medoids has been the optimal
-        else {
-            last_medoids.clear();
-            for (int i = 0; i < K; i ++) {
-                last_medoids.push_back(new_medoids[i]);
-            }
-            last_K = K;
-            last_cost = new_cost;
-        }
-        // STEP FIVE: M-step elect new K medoids as PAM algorithm
-        for (int i = 0; i < K; i ++) {
-            vector<int> cluster_points;
-            for (int j = 0; j < N; j ++) {
-                if (w[j][last_medoids[i]] > 0) 
-                    cluster_points.push_back(j);
-            }
+        // STEP FOUR: M-step elect new K medoids as PAM algorithm
+        int nClusters = new_medoids.size();
+        for (int c = 0; c < nClusters; c ++) {
+            vector<int> cluster_points = members[c];
             int nPoints = cluster_points.size();
             double ** cluster_dist_mat = mat_init(nPoints, nPoints);
             for (int p = 0; p < nPoints; p ++) {
@@ -121,28 +72,55 @@ double DP_MEDOIDS (double** dist_mat, int N, double lambda, double** W, vector<i
             }
             double* squared_dist = new double [nPoints];
             mat_sum_col(cluster_dist_mat, squared_dist, nPoints, nPoints);
-            int min_index = INTEGER_MAX;
+            int min_index = -1;
             double min_value = INF;
-            for (int j = 0; j < nPoints; j++) {
+            for (int j = 0; j < nPoints; j++) 
                 if (squared_dist[j] < min_value) {
                     min_index = j;
                     min_value = squared_dist[j];
                 }
-            }
             assert (min_index < nPoints);
-            new_medoids[i] = cluster_points[min_index];
+            new_medoids[c] = cluster_points[min_index];
             delete [] squared_dist;
             mat_free (cluster_dist_mat, nPoints, nPoints);
         }
 
+        // STEP THREE: compute cost
+        double loss = 0.0; 
+        for (int i = 0; i < N; i ++) 
+            loss += 0.5 * dist_mat[i][new_medoids[z[i]]];
+        double reg = new_medoids.size() * lambda;
+        new_cost = loss + reg;
+        cout << "loss: " << loss 
+             << ", reg: " << reg 
+             << ", new_cost: " << new_cost << endl;
+        if ( new_cost < objmin ) objmin = new_cost;
+        objmin_trace << omp_get_wtime()-start_time << " " << objmin << endl;
+        // STEP FOUR: stopping criteria
+        if (new_cost == last_cost) {
+            cout << "CLUSTERING COST: " << last_cost << endl;
+            cout << "Resulted medoids: ";
+            for (int c = 0; c < nClusters; c++) 
+                cout << new_medoids[c] << ",";
+            cout << endl;
+            break;
+        } // medoids has been the optimal
+        else {
+            last_medoids.clear();
+            last_medoids = new_medoids;
+            last_cost = new_cost;
+        }
     }
+    for (int i = 0; i < N; i ++) 
+        for (int j = 0; j < N; j++) 
+            if (j == new_medoids[z[i]])
+                W[i][j] = 1.0;
+            else 
+                W[i][j] = 0.0;
 
     // STEP SEVEN: put converged solution to destination W
-    mat_copy (w, W, N, N);
     for (int i = 0; i < last_medoids.size(); i ++) 
         medoids->push_back(last_medoids[i]);
-    // STEP EIGHT: memory recollection
-    mat_free (w, N, N);
 
     return last_cost;
 }
@@ -189,8 +167,7 @@ int main (int argc, char ** argv) {
 
     // pre-compute distance matrix
     dist_func df = L2norm;
-    double ** dist_mat = mat_init (N, N);
-    compute_dist_mat (data, dist_mat, N, D, df, true); 
+    random_shuffle (data.begin(), data.end());
 
     // Run sparse convex clustering
     double *** W = new double** [nRuns];
@@ -201,6 +178,11 @@ int main (int argc, char ** argv) {
     vector<int> min_medoids;
     int min_nMedoids;
     start_time = omp_get_wtime();
+    double ** dist_mat = mat_init (N, N);
+    compute_dist_mat (data, dist_mat, N, D, df, true); 
+    ofstream dmat_out ("dist_mat");
+    dmat_out << mat_toString(dist_mat, N, N);
+    dmat_out.close();
     for (int i = 0; i < nRuns; i++) {
         W[i] = mat_init (N, N);
         // INVOKE algorithm function
--- codebase/DP_MEDOIDS/DP_MEDOIDS.h
+++ codebase/DP_MEDOIDS/DP_MEDOIDS.h
@@ -1,5 +1,6 @@
 #include "../util.h"
 #include <cassert>
+#include <algorithm>
 #include <cmath>
 #include <omp.h>
 #define INTEGER_MAX 3000000
