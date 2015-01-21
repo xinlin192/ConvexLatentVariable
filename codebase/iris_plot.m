6625f8f761079dad2e4cc3f0e8fe1be14808fb37
En-Hsu Yen <a061105@gmail.com>
2015-01-21 01:42:33 -0600

    plots

--- codebase/iris_plot.m
+++ codebase/iris_plot.m
@@ -1,14 +1,14 @@
-markersize = 7;
-linewidth = 2;
 
-lambdas = [0.5 0.8 1 1.2 1.5 2 2.5 3 4 5 6 20 25 100 120 500];
-redplot = [13.0006 10.3995 10 7.99974 7 7 5.99914 5.006 4.006 4.00138 3 3 2 2 1 1];
-semilogx(lambdas, redplot, 'rx--', [1.5], [7], 'ks', 'markersize', markersize)
+lambdas = [1 1.2 1.5 2 2.5 3 4 5 6 20 25 100 120 500];
+redplot = [10 7.99974 7 7 5.99914 5.006 4.006 4.00138 3 3 2 2 1 1];
+semilogx(lambdas, redplot, 'rx--', [1.5], [7], 'ks', 'markersize', 18)
 hold on
-legend('fractions', 'integers', 'FontSize', 11)
-semilogx([1], [10], 'ks', [2], [7], 'ks', [6 20], [3 3], 'ks', [25 100], [2 2], 'ks',[120, 500], [1 1],'ks', 'markersize', 7)
-semilogx([1.5 2], [7 7], 'k', [6 20], [3 3], 'k', [25 100], [2 2], 'k', [120, 500], [1 1], 'k', 'linewidth', 2)
+legend('fractions', 'integers')
+semilogx([1], [10], 'ks', [2], [7], 'ks', [6 20], [3 3], 'ks', [25 100], [2 2], 'ks',[120, 500], [1 1],'ks', 'markersize', 18)
+semilogx([1.5 2], [7 7], 'k', [6 20], [3 3], 'k', [25 100], [2 2], 'k', [120, 500], [1 1], 'k', 'linewidth', 3)
 
-xlabel('lambda')
-ylabel('K')
-title('Iris')
+axis([0.8, 600, 0, 11])
+xlabel('lambda', 'Fontsize', 18)
+ylabel('K', 'Fontsize', 18)
+title('Iris', 'Fontsize', 18)
+set(gca, 'Fontsize', 25)
